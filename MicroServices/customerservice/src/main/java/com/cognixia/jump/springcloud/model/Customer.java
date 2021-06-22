83a294e35e223321077da8eb0c3ea647ee483549
pauliepj <pjpdisco@gmail.com>
2021-06-22 11:53:35 -0500

    fixed auto-generation of customer Id

--- MicroServices/customerservice/src/main/java/com/cognixia/jump/springcloud/model/Customer.java
+++ MicroServices/customerservice/src/main/java/com/cognixia/jump/springcloud/model/Customer.java
@@ -21,7 +21,7 @@ public class Customer implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	@Id//Pk
-	//@GeneratedValue(strategy=GenerationType.AUTO)
+	@GeneratedValue(strategy=GenerationType.AUTO)
 	@Column(name="CUSTOMERID")
 	Integer customerId;
 	
@@ -82,7 +82,7 @@ public class Customer implements Serializable{
 	}
 
 	public void setCustomerId(Integer customerId) {
-		this.customerId = customerId;
+		this.customerId = -1;
 	}
 
 	public String getCustomerName() {
